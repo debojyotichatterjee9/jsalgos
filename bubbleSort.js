d3ea2b9409cb608c9ef3b9ab68470bc6172df3d6
Debojyoti Chatterjee <debojyoti@glionconsulting.com>
2019-10-09 17:58:18 +0530

    bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,88 @@
+let sampleArray =
+    [90, 2, 100, 9, 39, 62, 75, 14, 93, 80,
+        // 48, 99, 61, 32, 4, 89, 30, 16, 10, 46,
+        // 96, 59, 76, 7, 50, 74, 86, 57, 71, 36,
+        // 55, 3, 15, 31, 53, 79, 28, 47, 84, 24,
+        // 27, 41, 26, 18, 63, 56, 5, 1, 54, 43,
+        // 13, 11, 91, 44, 58, 88, 97, 25, 81, 64,
+        // 6, 94, 34, 51, 12, 78, 66, 72, 35, 87,
+        // 82, 20, 73, 98, 21, 65, 40, 60, 67, 29,
+        // 45, 19, 37, 23, 42, 83, 92, 77, 95, 85,
+        69, 52, 8, 38, 68, 49, 17, 70, 33, 22];
+
+
+/*==============================================================FOR LOOP METHOD==============================================================*/
+console.time(`forLoopMethod`)
+let forLoopMethod = (sampleArray) => {
+    let len = sampleArray.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (sampleArray[j] > sampleArray[j + 1]) {
+                let tmp = sampleArray[j];
+                sampleArray[j] = sampleArray[j + 1];
+                sampleArray[j + 1] = tmp;
+            }
+        }
+    }
+    return sampleArray;
+};
+console.log(forLoopMethod(sampleArray));
+console.timeEnd(`forLoopMethod`);
+
+
+/*==============================================================DO-WHILE LOOP METHOD==============================================================*/
+console.time(`doWhileLoopMethod`);
+let doWhileLoopMethod = (inputArr) => {
+    let len = inputArr.length;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < len; i++) {
+            if (inputArr[i] > inputArr[i + 1]) {
+                let tmp = inputArr[i];
+                inputArr[i] = inputArr[i + 1];
+                inputArr[i + 1] = tmp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return inputArr;
+};
+console.log(doWhileLoopMethod(sampleArray));
+console.timeEnd(`doWhileLoopMethod`);
+
+
+/*==============================================================FOR EACH METHOD==============================================================*/
+console.time(`forEachMethod`)
+let forEachMethod = (sampleArray) => {
+    let len = sampleArray.length;
+    sampleArray.forEach(item => {
+        sampleArray.forEach((elem, index) => {
+            // [elem, index] = [elem, index]
+            if (sampleArray[index] > sampleArray[index + 1]) {
+                [sampleArray[index], sampleArray[index + 1]] = [sampleArray[index + 1], sampleArray[index]];
+            }
+        })
+    })
+    return sampleArray;
+};
+console.log(forEachMethod(sampleArray));
+console.timeEnd(`forEachMethod`);
+
+
+/*==============================================================ARRAY.MAP METHOD==============================================================*/
+console.time(`arrayMapMethod`)
+let arrayMapMethod = (sampleArray) => {
+    let len = sampleArray.length;
+    sampleArray.map(item => {
+        sampleArray.map((elem, index) => {
+            // [elem, index] = [elem, index]
+            if (sampleArray[index] > sampleArray[index + 1]) {
+                [sampleArray[index], sampleArray[index + 1]] = [sampleArray[index + 1], sampleArray[index]];
+            }
+        })
+    })
+    return sampleArray;
+};
+console.log(arrayMapMethod(sampleArray));
+console.timeEnd(`arrayMapMethod`);
