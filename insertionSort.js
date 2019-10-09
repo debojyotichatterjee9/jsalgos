9ca8c5e52631b59007ed7895a2954f3785f0827b
Debojyoti Chatterjee <debojyoti@glionconsulting.com>
2019-10-09 19:40:53 +0530

    added insertion sort

---
+++ insertionSort.js
@@ -0,0 +1,34 @@
+let sampleArray =
+    [90, 2, 100, 9, 39, 62, 75, 14, 93, 80,
+        // 48, 99, 61, 32, 4, 89, 30, 16, 10, 46,
+        // 96, 59, 76, 7, 50, 74, 86, 57, 71, 36,
+        // 55, 3, 15, 31, 53, 79, 28, 47, 84, 24,
+        // 27, 41, 26, 18, 63, 56, 5, 1, 54, 43,
+        // 13, 11, 91, 44, 58, 88, 97, 25, 81, 64,
+        // 6, 94, 34, 51, 12, 78, 66, 72, 35, 87,
+        // 82, 20, 73, 98, 21, 65, 40, 60, 67, 29,
+        // 45, 19, 37, 23, 42, 83, 92, 77, 95, 85,
+        69, 52, 8, 38, 68, 49, 17, 70, 33, 22];
+
+let operations = 0;
+
+/*==============================================================FOR & WHILE LOOP METHOD==============================================================*/
+console.log(`\n==For & While-Loop Method==`);
+console.time(`Execution Time`);
+let insertionSort = (sampleArray) => {
+    let length = sampleArray.length;
+    for (let i = 1; i < length; i++) {
+        let key = sampleArray[i];
+        let j = i - 1;
+        while (j >= 0 && sampleArray[j] > key) {
+            sampleArray[j + 1] = sampleArray[j];
+            j = j - 1;
+            operations++;
+        }
+        sampleArray[j + 1] = key;
+    }
+    return sampleArray;
+};
+console.log(insertionSort(sampleArray));
+console.log(`Operations: ${operations}`);
+console.timeEnd(`Execution Time`);
